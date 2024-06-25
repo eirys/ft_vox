b23b03da9774af089365e0a35c457976313d9718
eirys <etran@student.42.fr>
2024-06-25 16:03:39 +0200

    Decl update

--- src/engine/vox_decl.h
+++ src/engine/vox_decl.h
@@ -6,7 +6,7 @@
 /*   By: etran <etran@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/03 09:05:39 by etran             #+#    #+#             */
-/*   Updated: 2024/06/25 15:09:34 by etran            ###   ########.fr       */
+/*   Updated: 2024/06/25 16:02:29 by etran            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,11 @@
 # define ENABLE_CUBEMAP 0
 # define ENABLE_FRUSTUM_CULLING 0
 # define ENABLE_SHADOW_MAPPING 0
-# define ENABLE_SSAO 1
+# define ENABLE_SSAO 0
 
 #ifdef VOX_CPP
 
-# define ENABLE_HIGH_RES 1
+# define ENABLE_HIGH_RES 0
 
 # if !ENABLE_SKYBOX && ENABLE_CUBEMAP
     static_assert(false, "Cubemap cannot be enabled if skybox is disabled");
